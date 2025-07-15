88d726c87aae608448a8689517acb04764780161
Roman Snimschikov <romasesi@gmail.com>
2025-07-15 19:09:18 +0300

    feat: [dev header]

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+
+import { defineConfig } from 'vite'
+import path from 'path'
+
+export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        }
+    },
+    // Добавьте этот блок
+    css: {
+        preprocessorOptions: {
+            scss: {
+                includePaths: [
+                    path.resolve(__dirname, 'src')
+                ]
+            }
+        }
+    }
+})
