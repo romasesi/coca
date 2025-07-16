b6199d80f1a8614311dd7db9372c043397dc6273
Roman Snimschikov <romasesi@gmail.com>
2025-07-16 16:54:17 +0300

    prisset developers

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,20 @@
-
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import path from 'path'
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src')
-        }
-    },
-    // Добавьте этот блок
-    css: {
-        preprocessorOptions: {
-            scss: {
-                includePaths: [
-                    path.resolve(__dirname, 'src')
-                ]
-            }
-        }
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
     }
+  },
+  // Добавьте этот блок
+  css: {
+    preprocessorOptions: {
+      scss: {
+        includePaths: [
+          path.resolve(__dirname, 'src')
+        ]
+      }
+    }
+  }
 })
