c982d60ba5564d4ff0b1feb22c8b266a2e55ce02
Roman Snimschikov <romasesi@gmail.com>
2025-07-30 17:51:50 +0300

    Add hero swiper - 2

--- src/scripts/Swiper.js
+++ src/scripts/Swiper.js
@@ -1,19 +1,22 @@
 export const trending__swiper = new Swiper('.trending__swiper', {
   slidesPerView: 2,
-  spaceBetween: 16, // вместо column-gap!
+  spaceBetween: 32,
+
   breakpoints: {
     0: {
-      slidesPerView: 1,
+      slidesPerView: 2,
       spaceBetween: 8,
     },
+
     768: {
       slidesPerView: 2,
-      spaceBetween: 16,
+      spaceBetween: 32,
     }
   }
-  });
+});
 
-// export const reviews__swiper = new Swiper('.reviews__swiper', {
-//   slidesPerView: 1,
-// });
+export const reviews__swiper = new Swiper('.reviews__swiper', {
+  slidesPerView: 1,
+  spaceBetween: 1,
+});
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 import '@/styles/main.scss';
 import Header from "@/scripts/Header.js";
 import {trending__swiper} from '@/scripts/Swiper.js';
+import {reviews__swiper} from '@/scripts/Swiper.js'
 
 
 
